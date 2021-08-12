4cd0c4b960785049770539ef1a92a61a9397dece
Alexey Klochai <alexey@wizardondemand.com>
2021-08-12 14:33:01 +0200

    Fix up the API structure

--- fastify/index.js
+++ fastify/index.js
@@ -1,63 +1,26 @@
 const fastify = require('fastify')
 const path = require('path')
 const autoload = require('fastify-autoload')
-// const { PrismaClient } = require('@prisma/client')
-// Import required AWS SDK clients and commands for Node.js
+
 const { SESClient } = require('@aws-sdk/client-ses')
-const { SendEmailCommand } = require('@aws-sdk/client-ses')
+const { SendTemplatedEmailCommand,
+  GetTemplateCommand, 
+  CreateTemplateCommand } = require('@aws-sdk/client-ses')
 const { fromIni } = require('@aws-sdk/credential-provider-ini')
-// Set the AWS Region.
-const REGION = 'eu-central-1' // e.g. "us-east-1"
-// Create SES service object.
+const REGION = 'eu-central-1'
+
 const sesClient = new SESClient({
   credentials: fromIni({ profile: 'default' }),
   region: REGION
 })
 
-const paramsForNormalEmail = {
+const paramsForTemplatedEmail = {
   Destination: {
-    /* required */
-    CcAddresses: [
-      /* more items */
-    ],
-    ToAddresses: [
-      'ababentsov.de@gmail.com' // RECEIVER_ADDRESS
-      /* more To-email addresses */
-    ]
-  },
-  Message: {
-    /* required */
-    Body: {
-      /* required */
-      Html: {
-        Charset: 'UTF-8',
-        Data: 'HTML_FORMAT_BODY'
-      },
-      Text: {
-        Charset: 'UTF-8',
-        Data: 'TEXT_FORMAT_BODY'
-      }
-    },
-    Subject: {
-      Charset: 'UTF-8',
-      Data: 'Email from fastify'
-    }
-  },
-  Source: 'ababentsov@gmail.com', // SENDER_ADDRESS
-  ReplyToAddresses: [
-    /* more items */
-  ]
-}
-
-const paramsForTemplateEmail = {
-  Destination: {
-    /* required */
     CcAddresses: [
       /* more CC email addresses */
     ],
     ToAddresses: [
       'kingfisher@example.imap.cc' // RECEIVER_ADDRESS
-      /* more To-email addresses */
     ]
   },
   Source: 'nightjar@example.imap.cc', // SENDER_ADDRESS
@@ -75,17 +38,7 @@ const paramsForTemplateCreation = {
   }
 }
 
-async function sendEmail (params) {
-  try {
-    const data = await sesClient.send(new SendEmailCommand(params))
-    console.log('Success', data)
-    return data // For unit tests.
-  } catch (err) {
-    console.log('Error', err)
-  }
-};
-
-async function sendTemplateEmail (params) {
+async function sendTemplatedEmail (params) {
   try {
     const data = await sesClient.send(new SendTemplatedEmailCommand(params))
     console.log('Success.', data)
@@ -104,26 +57,29 @@ async function createTemplate (params) {
   }
 }
 
+async function createTemplateIfNotExists (params) {
+  try {
+    let queryParams = { TemplateName: params.Template.TemplateName }
+    let templateExists = await sesClient.send(new GetTemplateCommand(queryParams))
+  } catch (err) {
+    createTemplate(params)
+  }
+}
+
 const app = fastify({ logger: true })
 
 app.post('/notify', async (req, res) => {
-  const { signedIn, action, params } = req.body
-  switch (action) {
-    case 'send-template-email':
-      sendTemplateEmail(params)
-      res.send('send template email')
-      break
-    case 'send-email':
-      sendEmail(params)
-      res.send('send email')
-      break
-    case 'create-template':
-      createTemplate(params)
-      res.send('create template')
+  const { userId, event, params } = req.body
+  switch (event) {
+    case 'migration-confirmed':
+      sendTemplatedEmail(paramsForTemplatedEmail)
+      res.send('migration-confirmed email sent')
       break
     default:
-      res.send('Something went wrong')
+      res.send('event not configured')
   }
 })
+
+createTemplateIfNotExists(paramsForTemplateCreation);
 
 const server = app.listen(3000, () => console.log('🚀 Server ready at: http://localhost:3000'))
